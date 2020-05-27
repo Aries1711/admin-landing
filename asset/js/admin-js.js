2fc805608829699f698cf2bb1a21f6812964be08
aries1711 <levfreey17@gmail.com>
2020-05-27 17:02:03 +0700

    desain sidebar dan show hide

---
+++ asset/js/admin-js.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+$('.dash-btn').click(function(e){
+    $('.main-contain .main-app .sidebar-app .side-container .row-menu .row-child .dash-show').toggleClass("show");
+    console.log(e);    
+});
+
+});
+
